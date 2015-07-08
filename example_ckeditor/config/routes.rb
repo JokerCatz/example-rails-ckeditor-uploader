5a7c398f4ea7e08d049f209971e7760760ae486d
JokerCatz <lovingcatz@gmail.com>
2015-07-08 18:57:37 +0800

    fix layout to show js / css , add ckeditor support in textarea

--- example_ckeditor/config/routes.rb
+++ example_ckeditor/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       post :upload
     end
   end
+  root :to => 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
